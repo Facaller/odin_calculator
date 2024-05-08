ee57e98f85c6cf2c878ae992ed7c8d2190546b26
Facaller <ashwin.metal13@gmail.com>
2024-05-08 14:46:27 +0200

    Add btn element & e.listener and psuedocode

--- script.js
+++ script.js
@@ -3,6 +3,12 @@ let varOne;
 let varTwo;
 let operator;
 
+//event listeners
+let btn = document.querySelector('#btn');
+btn.addEventListener('click', () => {
+    console.log('this works')
+})
+
 //operations and equations.
 function add (a, b) {
     return a + b
